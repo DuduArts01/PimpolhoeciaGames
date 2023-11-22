bcf38ec6e9816aa8732d4b6519a7a72fbb7f1886
Eduardo <eduardodemedeirossiqueira@gmail.com>
2023-11-21 22:35:23 -0300

    hash_desing

---
+++ games/hash/hash.js
@@ -0,0 +1,25 @@
+var gamer = "X";
+
+let positions_win = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7], 
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+]
+
+function togame(cell){
+    if(cell.innerHTML == ""){
+        cell.innerHTML = gamer;
+
+        if(gamer == "X"){
+            gamer = "O"
+        } else{
+            gamer = "X"
+        }
+        
+    }
+}
